c7b76f14ae2aad4ed7408d47737a09a65fecbffc
Fran <fjarriaza@gmail.com>
2024-09-26 16:30:18 +0200

    100k data process  test passed

--- brd4_binding/src/generate_descriptors/descriptor_runner.py
+++ brd4_binding/src/generate_descriptors/descriptor_runner.py
@@ -12,16 +12,19 @@ from pipeline.remove_protecting_group import clean_bb1
 
 def main():
     # Silence RDKit deprecation warning
-    _block = BlockLogs()
+    _block = BlockLogs()  # noqa: F841
 
     brd4_df = read_data("/home/papafrita/Projects/" +
                         "brd4_binding/data/brd4_5p.parquet")
     brd4_df = clean_bb1(brd4_df)
+
+    rows = 100000
+
+    brd4_df = brd4_df.iloc[:rows, :]
+
     descriptors_list = get_descriptors()
 
     descriptor_df = make_descriptor_df(brd4_df)
-
-    ids_test = brd4_df.index[:100000]
 
     start_time = time.time()
     print("Starting data processing...")
@@ -31,7 +34,7 @@ def main():
     id_counter = 0
     temp_dict = {}
     failed_ids = []
-    for id in ids_test:
+    for id in brd4_df.index:
         try:
             temp_row = get_vector_from_id(original_df=brd4_df,
                                           id=id,
@@ -47,13 +50,12 @@ def main():
             current_time = time.time()
             delta_time = current_time - start_time
 
-            print("{} of {} processed.".format(id_counter, len(ids_test)))
-            print("{:.1f}%".format(id_counter/len(ids_test)*100))
+            print("{} of {} processed.".format(id_counter, len(brd4_df.index)))
+            print("{:.1f}%".format(id_counter/len(brd4_df.index)*100))
 
             if id_counter > 0:
-                factor = len(ids_test)/id_counter
-                print(time.strftime("ETA: %M minutes",
-                                    time.localtime(delta_time*factor)))
+                factor = ((len(brd4_df.index)-id_counter)/id_counter)
+                print("ETA: {:.1f} minutes".format(delta_time*factor/60))
             print("-"*10)
 
         id_counter += 1
